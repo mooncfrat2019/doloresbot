754782f3169be5f20ffb01a6367553e6d73c342f
Igor Kolomoytsev <i.kolomoytsev@vk.team>
2026-03-09 00:26:11 +0300

    Update confirmation

--- blocks/client/bot/eventsTypes.mjs
+++ blocks/client/bot/eventsTypes.mjs
@@ -1,10 +1,12 @@
 import {messageNew} from "./types/messageNew.mjs";
 import {messageAllow} from "./types/messageAllow.mjs";
 import {messageDeny} from "./types/messageDeny.mjs";
+import {config} from "../../../modules/config.mjs";
 
 export const eventsTypes = {
   confirmation: async () => {
-     return '764191d1';
+     const conf = config.bot_confirmation;
+     return conf || '63199864';
   },
   message_new: async (body) => {
       console.log('message_new')
--- modules/config.mjs
+++ modules/config.mjs
@@ -4,6 +4,7 @@ export const config = {
     221547354: [process.env.COMM_TOKEN, process.env.COMM_TOKEN1, process.env.COMM_TOKEN2],
   },
   bot_secret: process.env.BOT_SECRET,
+  bot_confirmation: process.env.BOT_CONFIRMATION,
   mysql_config: {
     host: process.env.DBHOST,
     user:  process.env.DBLOGIN,
